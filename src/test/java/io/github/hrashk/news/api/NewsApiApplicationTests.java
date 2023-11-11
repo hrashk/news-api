4f1dcccf16af3feb80f1bca651a1bb380753f952
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-11-11 17:23:56 +0100

    add author service tests

--- src/test/java/io/github/hrashk/news/api/NewsApiApplicationTests.java
+++ src/test/java/io/github/hrashk/news/api/NewsApiApplicationTests.java
@@ -1,9 +1,13 @@
 package io.github.hrashk.news.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = PostgreSQLInitializer.class)
 class NewsApiApplicationTests {
 
     @Test
---
+++ src/test/java/io/github/hrashk/news/api/authors/AuthorServiceTest.java
@@ -0,0 +1,26 @@
+package io.github.hrashk.news.api.authors;
+
+import io.github.hrashk.news.api.PostgreSQLInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ContextConfiguration(initializers = PostgreSQLInitializer.class)
+@Import(AuthorService.class)
+class AuthorServiceTest {
+    @Autowired
+    private AuthorService service;
+
+    @Test
+    void findAll() {
+        assertThat(service.findAll()).isNotEmpty();
+    }
+}
