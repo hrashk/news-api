1688b5a23f593e7f67e1b109efb660d3835cbcf0
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2023-11-21 11:17:41 +0100

    merge comment samples

--- src/test/java/io/github/hrashk/news/api/util/EntitySamples.java
+++ src/test/java/io/github/hrashk/news/api/util/EntitySamples.java
@@ -3,21 +3,16 @@ package io.github.hrashk.news.api.util;
 import io.github.hrashk.news.api.authors.Author;
 import io.github.hrashk.news.api.categories.Category;
 import io.github.hrashk.news.api.comments.Comment;
-import io.github.hrashk.news.api.comments.CommentSamples;
 import io.github.hrashk.news.api.news.News;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.context.TestComponent;
-import org.springframework.context.annotation.Import;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @TestComponent
-@Import(CommentSamples.class)
 @RequiredArgsConstructor
 public class EntitySamples {
-    private final CommentSamples commentSamples;
-
     public String baseUrl() {
         return "/api/v1/news";
     }
@@ -47,35 +42,59 @@ public class EntitySamples {
     }
 
     public News greatNews() {
-        List<Comment> comments = commentSamples.twoComments();
-        Author author = Author.builder().id(3L).build();
-
-        News news = News.builder()
+        News news = new News().toBuilder()
                 .id(validId())
-                .author(author)
                 .category(Category.builder().id(11L).build())
                 .headline("Great news")
                 .content("Lorem ipsum dolor")
-                .comments(comments)
                 .createdAt(LocalDateTime.parse("2011-10-13T00:00:00"))
                 .updatedAt(LocalDateTime.parse("2011-10-13T00:00:00"))
                 .build();
 
-        comments.forEach(c -> {
-            c.setNews(news);
-            c.setAuthor(author);
-        });
+        List<Comment> comments = twoComments();
+        Author author = new Author().toBuilder().id(3L).build();
+
+        comments.forEach(news::addComment);
+        comments.forEach(author::addComment);
+        author.addNews(news);
 
         return news;
     }
 
     public News sadNews() {
-        return News.builder()
+        return new News().toBuilder()
                 .id(22L)
                 .author(Author.builder().id(5L).build())
                 .category(Category.builder().id(13L).build())
                 .headline("Sad news")
                 .content("Dolor sit amet")
+                .createdAt(LocalDateTime.parse("2011-07-17T00:00:00"))
+                .updatedAt(LocalDateTime.parse("2011-08-21T00:00:00"))
+                .build();
+    }
+
+
+    public List<Comment> twoComments() {
+        Comment n1 = smiley(7L);
+
+        Comment n2 = smirky(22L);
+
+        return List.of(n1, n2);
+    }
+
+    public Comment smiley(Long id) {
+        return Comment.builder()
+                .id(id)
+                .text("Smiley comment")
+                .createdAt(LocalDateTime.parse("2011-10-13T00:00:00"))
+                .updatedAt(LocalDateTime.parse("2011-10-13T00:00:00"))
+                .build();
+    }
+
+    public Comment smirky(Long id) {
+        return Comment.builder()
+                .id(id)
+                .text("Simrky comment")
                 .createdAt(LocalDateTime.parse("2011-07-17T00:00:00"))
                 .updatedAt(LocalDateTime.parse("2011-08-21T00:00:00"))
                 .build();
