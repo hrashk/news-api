eb803f0b087ef9da21605b58a475c72d212f8cf0
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-10-30 20:16:05 +0100

    allow only admins to get all authors

--- src/main/java/io/github/hrashk/news/api/authors/web/AuthorController.java
+++ src/main/java/io/github/hrashk/news/api/authors/web/AuthorController.java
@@ -10,6 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class AuthorController {
     private final AuthorMapper mapper;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<AuthorListResponse> getAllAuthors(@ParameterObject @PageableDefault Pageable pageable) {
         List<Author> authors = service.findAll(pageable);
 
