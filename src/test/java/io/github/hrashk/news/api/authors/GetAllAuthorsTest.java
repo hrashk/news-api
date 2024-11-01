6d22e366f89cd5d9ad18c63da6ed72a3c5643b32
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-11-01 13:04:11 +0100

    parameterize forbidden test

--- src/test/java/io/github/hrashk/news/api/authors/GetAllAuthorsTest.java
+++ src/test/java/io/github/hrashk/news/api/authors/GetAllAuthorsTest.java
@@ -3,11 +3,17 @@ package io.github.hrashk.news.api.authors;
 import io.github.hrashk.news.api.Constants;
 import io.github.hrashk.news.api.authors.web.AuthorListResponse;
 import io.github.hrashk.news.api.util.ControllerTest;
+import io.github.hrashk.news.api.util.DataSeeder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -71,33 +77,21 @@ class GetAllAuthorsTest extends ControllerTest {
         );
     }
 
-    @Test
-    void forbiddenWhenNoRole() {
-        Author author = seeder.withoutRoles();
+    @ParameterizedTest(name="{1}")
+    @MethodSource("forbiddenUsers")
+    void forbiddenIfNotAdmin(Function<DataSeeder, Author> userProvider, String userType) {
+        Author a = userProvider.apply(seeder);
 
-        ResponseEntity<Map> response = rest.withBasicAuth(author.getUsername(), author.getPassword())
+        ResponseEntity<Map> response = rest.withBasicAuth(a.getUsername(), a.getPassword())
                 .getForEntity(Constants.AUTHORS_URL, Map.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
     }
 
-    @Test
-    void forbiddenWhenModerator() {
-        Author author = seeder.moderator();
-
-        ResponseEntity<Map> response = rest.withBasicAuth(author.getUsername(), author.getPassword())
-                .getForEntity(Constants.AUTHORS_URL, Map.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
-    }
-
-    @Test
-    void forbiddenWhenPlainUser() {
-        Author author = seeder.plainUser();
-
-        ResponseEntity<Map> response = rest.withBasicAuth(author.getUsername(), author.getPassword())
-                .getForEntity(Constants.AUTHORS_URL, Map.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    static Stream<Arguments> forbiddenUsers() {
+        return Stream.of(
+                Arguments.of((Function<DataSeeder, Author>) DataSeeder::withoutRoles, "no role"),
+                Arguments.of((Function<DataSeeder, Author>) DataSeeder::moderator, "moderator"),
+                Arguments.of((Function<DataSeeder, Author>) DataSeeder::plainUser, "user"));
     }
 }
