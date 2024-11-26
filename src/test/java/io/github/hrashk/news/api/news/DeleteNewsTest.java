d8f5cb9d8364fa6e229141264c6831ba10ba776b
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-11-26 17:54:37 +0100

    fix delete news tests

--- src/test/java/io/github/hrashk/news/api/news/DeleteNewsTest.java
+++ src/test/java/io/github/hrashk/news/api/news/DeleteNewsTest.java
@@ -3,9 +3,8 @@ package io.github.hrashk.news.api.news;
 import io.github.hrashk.news.api.Constants;
 import io.github.hrashk.news.api.exceptions.ErrorInfo;
 import io.github.hrashk.news.api.util.ControllerTest;
+import io.github.hrashk.news.api.util.Credentials;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -18,10 +17,12 @@ class DeleteNewsTest extends ControllerTest {
         var news = seeder.comments().get(0).getNews();
         Long newsId = news.getId();
 
-        ResponseEntity<Void> response = delete(Constants.NEWS_ID_URL, seeder.admin(), newsId);
+        Credentials a = seeder.admin();
+        ResponseEntity<Void> response = delete(Constants.NEWS_ID_URL, a, newsId);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
 
-        ResponseEntity<ErrorInfo> findResponse = rest.getForEntity(Constants.NEWS_ID_URL, ErrorInfo.class, newsId);
+        ResponseEntity<ErrorInfo> findResponse = rest.withBasicAuth(a.username(), a.password())
+                .getForEntity(Constants.NEWS_ID_URL, ErrorInfo.class, newsId);
         assertAll(
                 () -> assertThat(findResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND),
                 () -> assertThat(findResponse.getBody().message()).contains("News")
@@ -40,13 +41,12 @@ class DeleteNewsTest extends ControllerTest {
         );
     }
 
-    @ParameterizedTest
-    @CsvSource({Constants.NEWS_ID_URL})
-    void deleteWithInvalidUser(String url) {
-        Long newsId = seeder.news().get(0).getId();
-        Long userId = INVALID_ID;
+    @Test
+    void deleteOthersNews() {
+        News news = seeder.aNewsNotByAuthor(seeder.plainUserId());
+        Long newsId = news.getId();
 
-        ResponseEntity<ErrorInfo> response = delete(url, seeder.admin(), ErrorInfo.class, newsId);
+        ResponseEntity<ErrorInfo> response = delete(Constants.NEWS_ID_URL, seeder.plainUser(), ErrorInfo.class, newsId);
 
         assertAll(
                 () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN),
