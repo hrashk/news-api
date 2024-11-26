2aac8d7371e8312ab2ec027ef8cca5cbe46cd572
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-11-26 18:10:11 +0100

    add authz tests

--- src/test/java/io/github/hrashk/news/api/news/DeleteNewsTest.java
+++ src/test/java/io/github/hrashk/news/api/news/DeleteNewsTest.java
@@ -4,12 +4,19 @@ import io.github.hrashk.news.api.Constants;
 import io.github.hrashk.news.api.exceptions.ErrorInfo;
 import io.github.hrashk.news.api.util.ControllerTest;
 import io.github.hrashk.news.api.util.Credentials;
+import io.github.hrashk.news.api.util.HttpExecutable;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class DeleteNewsTest extends ControllerTest {
     @Test
@@ -52,5 +59,38 @@ class DeleteNewsTest extends ControllerTest {
                 () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN),
                 () -> assertThat(response.getBody().message()).contains("not allowed")
         );
+    }
+
+    @TestFactory
+    public List<DynamicTest> authorization() {
+        News news = seeder.news().get(0);
+        Credentials creds = seeder.newsAuthorCreds(news);
+
+        return List.of(
+                delById("no roles -> forbidden", seeder.withoutRoles(), HttpStatus.FORBIDDEN, news.getId()),
+                delById("anonymous -> unauthorized", null, HttpStatus.UNAUTHORIZED, news.getId()),
+                delById("wrong creds -> unauthorized", seeder.fakeUser(), HttpStatus.UNAUTHORIZED, news.getId()),
+                deleteOther("as user -> forbidden", seeder.plainUser(), HttpStatus.FORBIDDEN, seeder.plainUserId()),
+                delById("own news -> ok", creds, HttpStatus.NO_CONTENT, news.getId()),
+                deleteOther("as admin -> ok", seeder.admin(), HttpStatus.NO_CONTENT, seeder.adminId()),
+                deleteOther("as moderator -> ok", seeder.moderator(), HttpStatus.NO_CONTENT, seeder.moderatorId())
+        );
+    }
+
+    private DynamicTest deleteOther(String message, Credentials creds, HttpStatus status, Long authorId) {
+        var news = seeder.aNewsNotByAuthor(authorId);
+
+        return delById("another news " + message, creds, status, news.getId());
+    }
+
+    private DynamicTest delById(String message, Credentials creds, HttpStatus status, Long id) {
+        return dynamicTest(message, HttpExecutable.builder()
+                .method(HttpMethod.DELETE)
+                .url(Constants.NEWS_ID_URL)
+                .credentials(creds)
+                .expectedStatus(status)
+                .rest(rest)
+                .urlVariable(id)
+                .build());
     }
 }
